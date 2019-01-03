527c6eb8e6116f74fecda8f3039939d2cda834d8
Ruargh <ruargh@gmail.com>
2019-01-03 14:07:33 +0200

    p4-l1-7 Managing more State

--- index.js
+++ index.js
@@ -32,14 +32,53 @@ function createStore (reducer) {
 
 // App Code
 function todos (state = [], action) {
+
+  /* == Refactor to switch == */
+  /*
   if (action.type === 'ADD_TODO') {
     return state.concat([action.todo])
+  } else if (action.type === 'REMOVE_TODO') {
+    return state.filter((todo) => todo.id !== action.id)
+  } else if (action.type === 'TOGGLE_TODO') {
+    return state.map((todo) => todo.id !== action.id ? todo :
+    Object.assign({}, todo, { complete: !todo.complete }))
+  } else {
+    return state
   }
+  */
 
-  return state
+  switch(action.type) {
+    case 'ADD_TODO' :
+      return state.concat([action.todo])
+    case 'REMOVE_TODO' :
+      return state.filter((todo) => todo.id !== action.id)
+    case 'TOGGLE_TODO' :
+      return state.map((todo) => todo.id !== action.id ? todo :
+      Object.assign({}, todo, { complete: !todo.complete }))
+    default :
+      return state
+  }
 }
 
-const store = createStore(todos)
+function goals (state = [], action) {
+  switch(action.type) {
+    case 'ADD_GOAL' :
+      return state.concat([action.goal])
+    case 'REMOVE_GOAL' :
+      return state.filter((goal) => goal.id !== action.id)
+    default:
+      return state
+  }
+}
+
+function app (state = {}, action) {
+  return {
+    todos: todos(state.todos, action),
+    goals: goals(state.goals, action),
+  }
+}
+
+const store = createStore(app)
 store.subscribe(()=> {
   console.log('The new stae is ', store.getState())
 })
