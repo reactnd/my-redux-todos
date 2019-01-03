5e30cd49ca568ff038cbc7294611b512719c68bc
Ruargh <ruargh@gmail.com>
2019-01-03 12:55:46 +0200

    p4-l1-6 Putting it all together

--- index.js
+++ index.js
@@ -39,8 +39,19 @@ function todos (state = [], action) {
   return state
 }
 
+const store = createStore(todos)
+store.subscribe(()=> {
+  console.log('The new stae is ', store.getState())
+})
 
-
+store.dispatch({
+  type: 'ADD_TODO',
+  todo: {
+    id: 0,
+    name: 'Learn Redux',
+    complete: false,
+  }
+})
 
 
 /* Example code to call above functions
